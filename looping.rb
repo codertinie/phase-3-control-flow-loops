dd0ee719e6032162d231e50102fe57dbdf74fc54
codertinie <martin.munyao@moringaschool.com>
2022-10-17 19:20:55 +0300

    Completed assignment

--- looping.rb
+++ looping.rb
@@ -17,8 +17,29 @@ end
 
 def fizzbuzz_printer
   # your code here
+  # check each num from 1 to 100
+ (1..100).each do |num|
+  # print its fizzbuzz as defined above
+  puts fizzbuzz(num)
+ end
 end
 
 def reverse_string(str)
   # your code here
+  reversed_str = ""
+  i = 0
+  while i < str.length
+    reversed_str = str[i] + reversed_str
+    i += 1
+  end
+  reversed_str
+end
+
+def happy_new_year
+  counter = 10
+  until counter == 0
+    puts "#{counter}"
+    counter -= 1
+  end
+  puts "Happy New Year!"
 end
